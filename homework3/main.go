fff47c06f21431df32c386d0eb854f59bf245eb9
ZC <22128015@zju.edu.cn>
2022-01-11 17:46:03 +0800

    homework3 1.1.0 version

--- homework3/main.go
+++ homework3/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 var requestFloors []int
 
@@ -33,23 +36,35 @@ func (e *Elevator) elevatorMove(direction string, nowFloor int) {
 		for {
 			time.Sleep(1 * time.Second)
 			nowFloor++
+			e.nowFloor = nowFloor
 			if nowFloor == requestFloors[0] {
+				e.Open()
+				time.Sleep(1 * time.Second)
+				e.Close()
 				requestFloors = requestFloors[1:]
 				return
 			}
 		}
-
 	}
 	if direction == "向下" {
 		for {
 			time.Sleep(1 * time.Second)
 			nowFloor--
+			e.nowFloor = nowFloor
 			if nowFloor == requestFloors[0] {
+				e.Open()
+				time.Sleep(1 * time.Second)
+				e.Close()
 				requestFloors = requestFloors[1:]
 				return
 			}
 		}
-
 	}
 	return
+}
+func (e *Elevator) Open() {
+	fmt.Println("电梯开门")
+}
+func (e *Elevator) Close() {
+	fmt.Println("电梯关门")
 }
