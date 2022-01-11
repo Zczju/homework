3bc0a848a73e5246662323b0fdf817dbaa976c08
ZC <22128015@zju.edu.cn>
2022-01-11 16:51:32 +0800

    homework3

---
+++ homework3/main.go
@@ -0,0 +1,58 @@
+package main
+
+import "time"
+
+var requestFloors []int
+
+type Elevator struct {
+	floor     int
+	nowFloor  int
+	stopFloor int
+	direction string
+}
+
+func (e *Elevator) getDirection(requestFloors []int) string {
+	if len(requestFloors) == 0 {
+		e.direction = "不动"
+		return e.direction
+	} else {
+		if requestFloors[0] > e.nowFloor {
+			e.direction = "向上"
+			return e.direction
+		} else if requestFloors[0] < e.nowFloor {
+			e.direction = "向下"
+			return e.direction
+		}
+
+	}
+
+	return ""
+}
+
+func (e *Elevator) elevatorMove(direction string, nowFloor int) {
+	if direction == "向上" {
+		for {
+			time.Sleep(1 * time.Second)
+			nowFloor++
+			if nowFloor == requestFloors[0] {
+				e.stopFloor = nowFloor
+				requestFloors = requestFloors[1:]
+				return
+			}
+		}
+
+	}
+	if direction == "向下" {
+		for {
+			time.Sleep(1 * time.Second)
+			nowFloor--
+			if nowFloor == requestFloors[0] {
+				e.stopFloor = nowFloor
+				requestFloors = requestFloors[1:]
+				return
+			}
+		}
+
+	}
+	return
+}
---
+++ homework3/main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCase1(t *testing.T) {
+	e := &Elevator{
+		floor:    5,
+		nowFloor: 3,
+	}
+	requestFloors = []int{}
+	direction := e.getDirection(requestFloors) // 0表示无人请求电梯
+	if direction != "不动" {
+		t.Fatalf("预期电梯不动，但得到的却是：%s", direction)
+	}
+}
+
+func TestCase2(t *testing.T) {
+	e := &Elevator{
+		floor:    5,
+		nowFloor: 1,
+	}
+	requestFloors = append(requestFloors, 3)
+	direction := e.getDirection(requestFloors)
+	if direction != "向上" {
+		t.Fatalf("预期电梯向上，但得到的却是：%s", direction)
+	}
+	e.elevatorMove(e.direction, e.nowFloor)
+	if e.stopFloor != 3 {
+		t.Fatalf("预期电梯停在三楼，但得到的却是：%d", e.stopFloor)
+	}
+
+}
+
+func TestCase3(t *testing.T) {
+	e := &Elevator{
+		floor:    5,
+		nowFloor: 3,
+	}
+	requestFloors = append(requestFloors, 4, 2)
+	direction := e.getDirection(requestFloors)
+	if direction != "向上" {
+		t.Fatalf("预期电梯向上，但得到的却是：%s", direction)
+	}
+	e.elevatorMove(e.direction, e.nowFloor)
+	if e.stopFloor != 4 {
+		t.Fatalf("预期电梯停在四楼，但得到的却是：%d", e.stopFloor)
+	}
+
+	direction = e.getDirection(requestFloors)
+	if direction != "向下" {
+		t.Fatalf("预期电梯向下，但得到的却是：%s", direction)
+	}
+	e.elevatorMove(e.direction, e.nowFloor)
+	if e.stopFloor != 2 {
+		t.Fatalf("预期电梯停在四楼，但得到的却是：%d", e.stopFloor)
+	}
+
+}
