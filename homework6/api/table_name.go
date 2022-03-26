7c11badec94f0614467ee0b4d5259f5f37329e5d
ZC <22128015@zju.edu.cn>
2022-03-27 00:12:56 +0800

    homework 6

---
+++ homework6/api/table_name.go
@@ -0,0 +1,5 @@
+package api
+
+func (*PersonalShow) TableName() string {
+	return "personShow"
+}
---
+++ homework6/interface/interface.go
@@ -0,0 +1,27 @@
+package showInterface
+
+import "homework/homework6/api"
+
+type ServeInterface interface {
+	//保存show information
+	SaveShowInformation(ps *api.PersonalShow) error
+
+	//更新show information
+	UpdatePersonalInformation(ps *api.PersonalShow) error
+
+	//查询show information
+	GetShow() ([]*api.PersonalShow, error)
+
+	GetOneShow(name string) (*api.PersonalShow, error)
+
+	//真删除show information
+	DeleteTrue(id, personID int64) error
+
+	//假删除show information
+	//DeleteFalse(name string, Id, personID int64) error
+	DeleteFalse(Id, personID int64) error
+}
+
+type RankInitInterface interface {
+	Init() error
+}
